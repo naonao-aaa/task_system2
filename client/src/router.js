f4a558fa5d8f4f9d25a2ef19408022e1bd14834d
naonao-aaa <naonao1814naonao@gmail.com>
2023-01-03 15:48:03 +0900

    （戻し）動的なURL

--- client/src/router.js
+++ client/src/router.js
@@ -9,6 +9,6 @@ export default new Router({
   mode: "history",
   routes: [
     {path: '/', component: Home},
-    {path: '/users/:id', component: Users}
+    {path: '/users', component: Users}
   ]
 });
