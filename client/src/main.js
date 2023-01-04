cdd2351af8b0a845462d6d8e939cd8e25c18e54e
naonao-aaa <naonao1814naonao@gmail.com>
2023-01-04 23:29:37 +0900

    BootstrapとBoostrapVueをインストールし、使えるように設定

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 import App from './App.vue'
 import router from "./router";
 import store from "./store";
+// bootstrap-vueからBootstrapVue,IconPluginをインポートする
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Bootstrap と BootstrapVueのCSSファイルをインポート（順序はBootstrap⇒BootstrapVue）
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue, IconsPlugin);
 
 Vue.config.productionTip = false;
 
