a74ff22f46bc17e984dacf4fe4359a794f361e0c
naonao-aaa <naonao1814naonao@gmail.com>
2023-01-05 22:42:40 +0900

    調整

--- client/src/pages/category/CategoryCreate.vue
+++ client/src/pages/category/CategoryCreate.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <div class="card">
+        <div class="card border-info">
           <div class="card-header">カテゴリー新規作成画面</div>
 
           <div class="card-body">
