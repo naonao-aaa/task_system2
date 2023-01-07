d35ac6e99a54bae3bcdeff05e67c76bc5e0263bd
naonao-aaa <naonao1814naonao@gmail.com>
2023-01-08 08:46:08 +0900

    残りのVuex切り離し

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,41 +1,32 @@
 import Vue from 'vue';       
 import Vuex from 'vuex';
-import axios from 'axios';
+//import axios from 'axios';
 import category from "./modules/category.js";
 import task from "./modules/task.js";
 import user from "./modules/user.js";
+import status from "./modules/status.js";
 import comment from "./modules/comment.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        statusList: [],
+        
     },
     getters: {
-        statusList: state => state.statusList,
+        
     },
     mutations: {
-        updateStatusList(state, newStatusList) {
-            state.statusList = newStatusList;
-        },
+        
     },
     actions: {
-        updateStatusList({ commit }) {
-            axios.get(
-                '/api/status/index'
-            )
-            .then(response => {
-                console.log(response);
-                const newStatusList = response.data.statuses;
-                commit('updateStatusList', newStatusList);
-            });
-        },
+        
     },
     modules: {
         category,
         task,
         user,
+        status,
         comment
     }
 });
---
+++ client/src/store/modules/status.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const state = {
+    statusList: [],
+};
+
+const getters = {
+    statusList: state => state.statusList,
+};
+
+const mutations = {
+    updateStatusList(state, newStatusList) {
+        state.statusList = newStatusList;
+    },
+};
+
+const actions = {
+    updateStatusList({ commit }) {
+        axios.get(
+            '/api/status/index'
+        )
+        .then(response => {
+            console.log(response);
+            const newStatusList = response.data.statuses;
+            commit('updateStatusList', newStatusList);
+        });
+    },
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
