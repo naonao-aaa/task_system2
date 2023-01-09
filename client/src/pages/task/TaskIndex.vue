10dda49ee4623c3bdeef99e81e38ee5c0e019f33
naonao-aaa <naonao1814naonao@gmail.com>
2023-01-09 12:25:42 +0900

    【ページネーション設定】タスク絞り込み時に、ページネーション部分が連動して少なく表示されるように

--- client/src/pages/task/TaskIndex.vue
+++ client/src/pages/task/TaskIndex.vue
@@ -19,7 +19,7 @@
                     <th scope="col">更新日時</th>
                     </tr>
                 </thead>
-                <tbody v-for="task in computedTasks" :key="task.id">
+                <tbody v-for="task in afterSliceComputedTasksForPaginate" :key="task.id">
                 <tr @click="goShow(task.id)">
                     <td>{{task.id}}</td>
                     <td>{{task.status ? task.status.name : ''}}</td>
@@ -64,12 +64,9 @@ export default {
 
             //console.log(this.$route.query.category);
 
-            let current = this.currentPage * this.perPage;
-            let start = current - this.perPage;
-
             if ((this.$route.query.category == 0 && this.$route.query.status == 0 && this.$route.query.user == 0 && this.$route.query.searchword == '') || (!this.$route.query.category && !this.$route.query.status && !this.$route.query.user && !this.$route.query.searchword)) {
-                console.log(getters);
-                return getters.slice(start, current);
+                //console.log(getters);
+                return getters;
             } else {
                 const categoryDataId = parseInt(this.$route.query.category, 10);
                 const statusDataId = parseInt(this.$route.query.status, 10);
@@ -86,24 +83,26 @@ export default {
                 if( searchword=='' || !searchword ) {
                     //console.log('searchwordなしの処理を通っている');
                     const finalData = tentativeData
-                    return finalData.slice(start, current);
+                    return finalData;
                 } else {
                     const finalData = tentativeData.filter( function(b) {
                         //console.log('searchwordありの処理を通っている');
                         return b.name.match(searchword);
                     })
-                    return finalData.slice(start, current);
+                    return finalData;
                 }
                 //console.log(finalData);
                 //return finalData;
             }
         },
-        rawGettersTaskList() {
-            return this.$store.getters.taskList;
+        afterSliceComputedTasksForPaginate() {
+            let current = this.currentPage * this.perPage;
+            let start = current - this.perPage;
+            return this.computedTasks.slice(start, current);
         },
         getPageCount() {
             //console.log(this.computedTasks.length);
-            return Math.ceil(this.rawGettersTaskList.length / this.perPage);
+            return Math.ceil(this.computedTasks.length / this.perPage);
         }
     },
     created() {
