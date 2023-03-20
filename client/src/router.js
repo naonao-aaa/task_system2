327dae87bf2fed23beb223c71385400410c797eb
naonao-aaa <naonao1814naonao@gmail.com>
2023-03-20 11:02:15 +0900

    beforeEnterを用いて、表示されてはいけない画面に飛ばないように調整

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Test from "./views/MyTest.vue";
+import store from './store';
 import TaskIndex from "@/pages/task/TaskIndex.vue";
 import TaskCreate from "@/pages/task/TaskCreate.vue";
 import TaskShow from "@/pages/task/TaskShow.vue";
@@ -28,31 +28,150 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   routes: [
+    //ログイン
     {path: '/login', component: MyLogin, name: "MyLogin"},
 
+    //タスク
     {path: '/task/index', component: TaskIndex, name: "TaskIndex"},
     {path: '/task/create', component: TaskCreate, name: "TaskCreate"},
     {path: '/task/show/:id', component: TaskShow, name: "TaskShow"},
     {path: '/task/edit/:id', component: TaskEdit, name: "TaskEdit"},
     {path: '/task/destroy/:id', component: TaskDestroy, name: "TaskDestroy"},
 
+    //ユーザー
     {path: '/user/index', component: UserIndex, name: "UserIndex"},
-    {path: '/user/create', component: UserCreate, name: "UserCreate"},
-    {path: '/user/edit/:id', component: UserEdit, name: "UserEdit"},
-    {path: '/user/edit/password/:id', component: PasswordEdit, name: "PasswordEdit"},
-    {path: '/user/destroy/:id', component: UserDestroy, name: 'UserDestroy'},
+    {
+      path: '/user/create', 
+      component: UserCreate, 
+      name: "UserCreate",
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/user/index');
+        }
+      }
+    },
+    {
+      path: '/user/edit/:id', 
+      component: UserEdit, 
+      name: "UserEdit",
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else if (to.params.id != store.getters.loginUser.id) {   //ログインユーザーと一致しない時
+          next('/user/index');
+        }
+        next();
+      },
+    },
+    {
+      path: '/user/edit/password/:id', 
+      component: PasswordEdit, 
+      name: "PasswordEdit",
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else if (to.params.id != store.getters.loginUser.id) {    //ログインユーザーと一致しない時
+          next('/user/index');
+        }
+        next();
+      }
+    },
+    {
+      path: '/user/destroy/:id', 
+      component: UserDestroy, 
+      name: 'UserDestroy',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else if (to.params.id != store.getters.loginUser.id) {    //ログインユーザーと一致しない時
+          next('/user/index');
+        }
+        next();
+      }
+    },
 
+    //カテゴリー
     {path: '/category/index', component: CategoryIndex, name: 'CategoryIndex'},
-    {path: '/category/create', component: CategoryCreate, name: 'CategoryCreate'},
-    {path: '/category/edit/:id', component: CategoryEdit, name: 'CategoryEdit'},
-    {path: '/category/destroy/:id', component: CategoryDestroy, name: 'CategoryDestroy'},
+    {
+      path: '/category/create', 
+      component: CategoryCreate, 
+      name: 'CategoryCreate',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/category/index');
+        }
+      }
+    },
+    {
+      path: '/category/edit/:id', 
+      component: CategoryEdit, 
+      name: 'CategoryEdit',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/category/index');
+        }
+      }
+    },
+    {
+      path: '/category/destroy/:id', 
+      component: CategoryDestroy, 
+      name: 'CategoryDestroy',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/category/index');
+        }
+      }
+    },
 
+    //ステータス
     {path: '/status/index', component: StatusIndex, name: 'StatusIndex'},
-    {path: '/status/create', component: StatusCreate, name: 'StatusCreate'},
-    {path: '/status/edit/:id', component: StatusEdit, name: 'StatusEdit'},
-    {path: '/status/destroy/:id', component: StatusDestroy, name: 'StatusDestroy'},
+    {
+      path: '/status/create', 
+      component: StatusCreate, 
+      name: 'StatusCreate',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/status/index');
+        }
+      }
+    },
+    {
+      path: '/status/edit/:id', 
+      component: StatusEdit, 
+      name: 'StatusEdit',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/status/index');
+        }
+      }
+    },
+    {
+      path: '/status/destroy/:id', 
+      component: StatusDestroy, 
+      name: 'StatusDestroy',
+      beforeEnter(to, from, next) {
+        if(store.getters.loginUser.admin == true) {    //管理者権限
+          next();
+        } else {  
+          next('/status/index');
+        }
+      }
+    },
     
-    {path: '/mytest', component: Test},
+
+    //その他
     {path: '*', redirect: '/login'},
   ]
 });
